5b3983f230fb3e49db813c26cc8879536011c9a3
Thiago Piacentini <piacentinithiago83@gmail.com>
2023-09-19 20:36:00 +0800

    nextjs with storybook

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
+const path = require('path'); // Importe o módulo path
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+}
 
 module.exports = nextConfig
